f647e79d893bdb15feba49f647f394845d43956e
Patrick Wigger <pwg@Patricks-MacBook-Air.local>
2017-06-06 09:42:49 +0200

    added Guava

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
 /**
  * Created by pwg on 24.05.17.
  */
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Dist Bonusaufgabe");
+    HashFunction hf=Hashing.murmur3_32((int)Math.random()*100);
+    System.out.println(hf.hashInt(34));
+
   }
+
+
 }
